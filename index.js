1e1715190dc78e76bc0e1770aa4f604aefae4583
owo-del <123456789+trowom001@protonmail.com>
2023-09-20 19:43:45 +0800

    - added randomized colors - improve game performance by removing the cause of accumulated event

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const DEFAULT_COLOR = '#e4e4e4';
 
-const container = document.getElementById("container");
+const container = document.getElementById('container');
 const btnResize = document.getElementById('btnGridResize');
+const btnRandomColor = document.getElementById('btnRandomColor');
+
 
 function gridResize(gridTiles){
     container.style.gridTemplateColumns = `repeat(${gridTiles}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridTiles}, 1fr)`;
     
+    container.innerHTML = ''; // clear container to avoid lag
+
     for (let i = 0; i < gridTiles * gridTiles; i++){
         const createDiv = document.createElement("div");
         createDiv.classList.add("square-box");
         container.appendChild(createDiv);
-    };    
-}
-
-function hovering(){
-    const boxColored = document.querySelectorAll(".square-box");
-    boxColored.forEach((box) => {
-        box.addEventListener("mouseover", () => {
-            box.style.backgroundColor = "#4b4b4b";
+    
+        createDiv.addEventListener("mouseover", () => {
+            createDiv.style.backgroundColor = "#4b4b4b";
         });
-        
-        box.addEventListener("mouseout", () => {
+
+        createDiv.addEventListener("mouseout", () => {
             setTimeout(() => {
-                box.style.backgroundColor = "#e4e4e4";
+                createDiv.style.backgroundColor = DEFAULT_COLOR;
             }, 5000);
         })
-    });
-}
+    }
+};
 
 function resetColor(){
     const boxReset = document.querySelectorAll(".square-box");
@@ -36,19 +35,41 @@ function resetColor(){
     });
 }
 
+
+function randomColor(){
+    const boxColored = document.querySelectorAll(".square-box");
+    boxColored.forEach((box) => {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+    
+        let randomColor = `rgb(${red}, ${green}, ${blue})`;
+    
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = randomColor;
+        });
+        box.addEventListener('mouseout', () => {
+            setTimeout(() => {
+                box.style.backgroundColor = DEFAULT_COLOR;
+            }, 5000);
+        });
+    });
+    console.log(`Random color: ${randomColor}`);
+}
+
 gridResize(16); //default
-hovering();
 
 btnResize.addEventListener('click', () => {
-    sides = prompt("Number of squares per side:");
+    sides = prompt("Grid size (1 - 100) :");
     resetColor();
     if (sides <= 100){
         gridResize(sides);
-        hovering();
         console.log(sides);
     } else {
         alert("100 and below only.");
     }
 })
 
-
+btnRandomColor.addEventListener('click', () => {
+    randomColor();
+});
